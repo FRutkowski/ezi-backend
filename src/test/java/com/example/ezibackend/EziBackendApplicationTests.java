1290af244dcc650c879a26925a66109968143308
Patrycja Rećko <patrycjarecko00@gmail.com>
2023-12-02 14:28:51 +0100

    feat: created repository

---
+++ src/test/java/com/example/ezibackend/EziBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ezibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EziBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
