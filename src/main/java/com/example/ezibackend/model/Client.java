df8ae05529aea99dd3156619772037cf9528d110
SiwyKot41 <frutkowski80@gmail.com>
2024-01-02 16:33:17 +0100

    feat: add client model

---
+++ src/main/java/com/example/ezibackend/model/Client.java
@@ -0,0 +1,28 @@
+package com.example.ezibackend.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "CLIENT")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "secondName")
+    private String secondName;
+
+    @Column(name = "address")
+    private String address;
+}
